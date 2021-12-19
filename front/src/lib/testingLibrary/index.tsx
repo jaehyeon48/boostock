ba5c40b0d0d668706d2b687cccf1af7008d02e2f
jaehyeon48 <imjaehyeon48@gmail.com>
2021-12-19 23:15:56 +0900

    fix: customRender 함수들 수정
    BrowserRouter를 Wrapper로 추가

--- front/src/lib/testingLibrary/index.tsx
+++ front/src/lib/testingLibrary/index.tsx
@@ -1,4 +1,5 @@
 import { ReactElement } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import { render, RenderOptions } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
@@ -7,17 +8,27 @@ import Socket from '@src/Socket';
 function RecoilWrapper({ children }: { children: ReactElement }) {
 	return (
 		<RecoilRoot>
-			<Socket>{children}</Socket>
+			<BrowserRouter>{children}</BrowserRouter>
+		</RecoilRoot>
+	);
+}
+
+function SocketWrapper({ children }: { children: ReactElement }) {
+	return (
+		<RecoilRoot>
+			<Socket>
+				<BrowserRouter>{children}</BrowserRouter>
+			</Socket>
 		</RecoilRoot>
 	);
 }
 
 function renderWithRecoil(ui: ReactElement, options?: RenderOptions) {
-	render(ui, { wrapper: RecoilRoot, ...options });
+	render(ui, { wrapper: RecoilWrapper, ...options });
 }
 
 function renderWithRecoilAndSocket(ui: ReactElement, options?: RenderOptions) {
-	render(ui, { wrapper: RecoilWrapper, ...options });
+	render(ui, { wrapper: SocketWrapper, ...options });
 }
 
 export * from '@testing-library/react';
