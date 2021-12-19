db1a122f1ac1ee13ad86b2d1e35823315cb45668
jaehyeon48 <imjaehyeon48@gmail.com>
2021-12-19 22:30:38 +0900

    remove: App.test.tsx 파일 제거

--- front/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { customRender, screen } from '@lib/testingLibrary';
-import App from './App';
-
-test('App component test', () => {
-	customRender(<App />);
-
-	const loginLink = screen.getByRole('link', { name: '로그인' });
-	expect(loginLink).toBeInTheDocument();
-});
