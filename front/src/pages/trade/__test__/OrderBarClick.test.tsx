1587194f69baad610b802c102331d4d572091665
jaehyeon48 <imjaehyeon48@gmail.com>
2021-12-29 15:06:45 +0900

    feat: 호가막대를 클릭했을 때 해당 막대의 가격을 주문가격 input 필드에 입력하는 동작에 대한 테스트 추가

---
+++ front/src/pages/trade/__test__/OrderBarClick.test.tsx
@@ -0,0 +1,30 @@
+import { renderWithRecoil, screen, userEvent } from '@lib/testingLibrary';
+import OrderBars from '../orderBars/OrderBars';
+import Order from '../order/Order';
+
+test('Type order price when a user clicks an order bar', async () => {
+	renderWithRecoil(
+		<>
+			<OrderBars previousClose={0} />
+			<Order stockCode="HNX" />
+		</>
+	);
+
+	const askAmountBars = await screen.findAllByLabelText('ask-amount-bar');
+	const askPriceBars = await screen.findAllByLabelText('ask-price-bar');
+	const bidAmountBars = await screen.findAllByLabelText('bid-amount-bar');
+	const bidPriceBars = await screen.findAllByLabelText('bid-price-bar');
+
+	const orderPriceInputBox = await screen.findByLabelText('Order price input box');
+	userEvent.click(askAmountBars[0]);
+	expect(orderPriceInputBox).toHaveValue('39,890,000');
+
+	userEvent.click(askPriceBars[0]);
+	expect(orderPriceInputBox).toHaveValue('39,890,000');
+
+	userEvent.click(bidAmountBars[0]);
+	expect(orderPriceInputBox).toHaveValue('39,240,000');
+
+	userEvent.click(bidPriceBars[0]);
+	expect(orderPriceInputBox).toHaveValue('39,240,000');
+});
