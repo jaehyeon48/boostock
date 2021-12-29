345b223f950df86a6b95dba573968b9a48e4fd9e
jaehyeon48 <imjaehyeon48@gmail.com>
2021-12-29 11:10:46 +0900

    feat: beforeAll 훅에 process.env.SERVER_URL과 Element.prototype.scrollTo 추가

--- front/src/setupTests.ts
+++ front/src/setupTests.ts
@@ -1,7 +1,12 @@
 import '@testing-library/jest-dom/extend-expect';
+import 'jest-canvas-mock';
 import { server } from '@mocks';
 
-beforeAll(() => server.listen());
+beforeAll(() => {
+	server.listen();
+	process.env.SERVER_URL = 'http://localhost:3000';
+	Element.prototype.scrollTo = () => {};
+});
 
 afterEach(() => server.resetHandlers());
 
